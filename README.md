ebaf1f860a01e27598bb5a3b83a3eea7661eaf2e
성식 채 <seongsik.chae@outlook.com>
2023-01-19 16:19:30 +0900

    Q1 removed

--- README.md
+++ README.md
@@ -40,32 +40,13 @@ npm run start
 
 과제에 대한 상세내용은 PPT 파일 참조
 
-### 과제 1
-
-이벤트 유형과 이벤트 필드 CRUD 화면 구현
-
-* (1)과 (2)를 탭 UI로 구성
-* (1)과 (2)의 사용된 테이블 UI를 공통 컴포넌트로 구현
-
-#### (1) 이벤트 유형 CRUD
-
-* /src/store/Mockup.js에 eventTypeList Mockup Data를 참조하여 이벤트 유형 CRUD 하는 화면 구현
-* 이벤트 유형 추가 기능은 추가 버튼 클릭 시 Modal 로 구현
-* 동일한 유형 ID로 이벤트 유형이 존재할 수 없음
-* 삭제 시 삭제할 것인지 여부 확인 후 삭제
-
-#### (2) 이벤트 필드 CRUD
-
-* /src/store/Mockup.js에 eventFieldList Mockup Data를 참조하여 이벤트 필드 CRUD 하는 화면 구현
-* 이벤트 필드 추가 기능은 추가 버튼 클릭 시 Modal 로 구현
-* 동일한 필드명으로 이벤트 필드가 존재할 수 없음
-* 삭제 시 삭제할 것인지 여부 확인 후 삭제
-
 ### 과제 2
 
 Directory 탐색기 구현
 
 * /src/store/Mockup.js 에 directories Mockup Data 를 참조하여 Directory 탐색기 구현
+* Mockup Data 를 Redux State 로 관리
+* Mockup Data 에서 서버 정보를 기반으로 서버를 선택 했을 때 해당 서버에 맞는 Directory 탐색기 가 표현되도록 구현 ( 첫 화면에서 서버 선택 하는 화면을 구현, 서버를 선택 하면 선택한 서버에 맞는 Directory 화면이 표시 되도록 구현 )
 * Directory 추가 및 File 추가 기능은 추가 버튼 클릭 시 Modal 로 구현
 * 동일한 경로에 동일한 이름의 Directory 및 File 이 존재할 수 없음
 * Directory 수정 시 Directory 이름만 수정
--- src/store/Mockup.js
+++ src/store/Mockup.js
@@ -1,68 +1,58 @@
 export const MockupState = {
-    eventTypeList: [
-        {
-            type_code: "AGENT",
-            type_name: "AGENT"
-        },
-        {
-            type_code: "DB",
-            type_name: "DB"
-        },
-        {
-            type_code: "LOGGATHER",
-            type_name: "LOGGATHER"
-        },
-        {
-            type_code: "SYSLOG",
-            type_name: "SYSLOG"
-        },
-        {
-            type_code: "access_log",
-            type_name: "[관리] 접근 로그"
-        }
-    ],
-    eventFieldList: [
-        {
-            field_name: "Action",
-            field_label: "허용 / 차단"
-        },
-        {
-            field_name: "ActualFree",
-            field_label: "ActualFree"
-        },
-        {
-            field_name: "ApplicationTitle",
-            field_label: "어플리케이션"
-        },
-        {
-            field_name: "Direction",
-            field_label: "방향"
-        },
-        {
-            field_name: "Dst",
-            field_label: "타겟 IP"
-        }
-    ],
     directories: [
         {
-            name: '/',
-            type: 'DIRECTORY',
-            parent: undefined,
-            children: [
-                {
-                    name: 'home',
-                    type: 'DIRECTORY',
-                    parent: '/',
-                    children: [
-                        {
-                            name: '.bash_history',
-                            type: 'FILE',
-                            file_size: 32819,
-                            modified_date: 1674035700000
-                        }
-                    ]
-                }
-            ]
+            id: 1,
+            serverName: 'Server #1',
+            directories: {
+                name: '/',
+                type: 'DIRECTORY',
+                parent: undefined,
+                children: [
+                    {
+                        name: 'home',
+                        type: 'DIRECTORY',
+                        parent: '/',
+                        children: [
+                            {
+                                name: '.bash_history',
+                                type: 'FILE',
+                                file_size: 32819,
+                                modified_date: 1674035700000
+                            }
+                        ]
+                    }
+                ]
+            }
+        },
+        {
+            id: 1,
+            serverName: 'Server #2',
+            directories: {
+                name: '/',
+                type: 'DIRECTORY',
+                parent: undefined,
+                children: []
+            }
+        },
+        {
+            id: 1,
+            serverName: 'Server #3',
+            directories: {
+                name: '/',
+                type: 'DIRECTORY',
+                parent: undefined,
+                children: []
+            }
+        },
+        {
+            id: 1,
+            serverName: 'Server #4',
+            directories: {
+                name: '/',
+                type: 'DIRECTORY',
+                parent: undefined,
+                children: []
+            }
         }
     ]
 }
